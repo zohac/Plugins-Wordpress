b63ea83396f5f5110a3d2948e7aaf3ef492c3ce0
zohac <fenrir0680@gmail.com>
2019-02-19 20:19:22 +0100

    Association Management init

---
+++ association-management/association-management.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: Association Management
+Plugin URI: https://jouan.ovh/
+Description: Ceci est mon premier plugin
+Author: Simon JOUAN
+Version: 1.0
+Author URI: https://jouan.ovh/
+*/
+
+// Ajouter un rôle d'utilisateur personnalisé
+add_role('adherent', __('Adhérent'), []);
